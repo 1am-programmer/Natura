8db6f355f30e2fa20a09a09cead80a2a105fee2a
Danny <dannydotdev@gmail.com>
2025-02-12 16:31:12 +0100

    User auth page

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, View } from "react-native";
+
+const login = () => {
+  return (
+    <SafeAreaView>
+      <View>Login page</View>
+    </SafeAreaView>
+  );
+};
+
+export default login;
---
+++ app/signUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, View } from "react-native";
+
+const Signup = () => {
+  return (
+    <SafeAreaView>
+      <View>Login page</View>
+    </SafeAreaView>
+  );
+};
+
+export default Signup;
