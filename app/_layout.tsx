b0188b362144ed2b8ba3611f0df45eeb9f381c9e
Danny <dannydotdev@gmail.com>
2025-03-09 15:11:40 +0100

    splahscreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,34 @@
-import Onboarding from "@/components/Onboarding";
+import { useEffect, useState } from "react";
+import { View, Image, StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function loadApp() {
+      await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate loading
+      setIsReady(true);
+      await SplashScreen.hideAsync();
+    }
+
+    loadApp();
+  }, []);
+
+  if (!isReady) {
+    return (
+      <View style={styles.splashContainer}>
+        <Image
+          source={require("@/assets/images/Splash.png")}
+          style={styles.splashImage}
+        />
+      </View>
+    );
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -11,3 +38,17 @@ export default function RootLayout() {
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#232323",
+  },
+  splashImage: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+});
